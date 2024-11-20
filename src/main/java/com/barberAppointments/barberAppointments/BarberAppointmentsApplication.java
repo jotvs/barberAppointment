54de7f17aa9e7644f7aa08e878eaa600db6fe76d
Joao Victor <joao.vslima@hotmail.com>
2024-11-20 20:31:13 -0300

    Primeiro commit

---
+++ src/main/java/com/barberAppointments/barberAppointments/BarberAppointmentsApplication.java
@@ -0,0 +1,14 @@
+package com.barberAppointments.barberAppointments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberAppointmentsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BarberAppointmentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barberAppointments/barberAppointments/BarberAppointmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barberAppointments.barberAppointments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberAppointmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
